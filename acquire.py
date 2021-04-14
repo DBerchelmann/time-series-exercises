099e05f9c716db8b9d9c797983a1287358edb100
David Berchelmann <david.a.berchelmann@gmail.com>
2021-04-13 20:29:49 -0500

    upload of files and completed solutions for acquire lesson

---
+++ acquire.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import requests
+
+
+def items():
+    
+    '''This function brings in the items contenct dictionary from all pages at the 
+       specified website, and then transforms it to a dataframe which it then returns'''
+    
+    items_list = []
+    url = "https://python.zach.lol/api/v1/items"
+
+    response = requests.get(url)
+    data = response.json()
+    n = data['payload']['max_page']
+
+
+    for i in range(1, n+1):
+        new_url = url+ '?page=' + str(i)
+        response = requests.get(new_url)
+        data = response.json()
+        page_items = data['payload']['items']
+        items_list += page_items
+        
+    items = pd.DataFrame.from_dict(items_list)
+    
+    return items
+    
+    
+def stores():
+    
+    '''This function brings in the stores content dictionary from all pages at the 
+       specified website, and thentransforms it to a dataframe which it then returns'''
+    
+    stores_list = []
+    url = "https://python.zach.lol/api/v1/stores"
+
+    response = requests.get(url)
+    data = response.json()
+    n = data['payload']['max_page']
+
+
+    for i in range(1, n+1):
+        new_url = url+ '?page=' + str(i)
+        response = requests.get(new_url)
+        data = response.json()
+        page_stores = data['payload']['stores']
+        stores_list += page_stores
+    
+    stores = pd.DataFrame.from_dict(stores_list)
+    
+    return stores
+
+#~~Run this function if you haven't already acquired the sales data already~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def sales_acquire():
+    sales_list = []
+    url = "https://python.zach.lol/api/v1/sales"
+
+    response = requests.get(url)
+    data = response.json()
+    n = data['payload']['max_page']
+
+
+    for i in range(1, n+1):
+        new_url = url+ '?page=' + str(i)
+        response = requests.get(new_url)
+        data = response.json()
+        page_sales = data['payload']['sales']
+        sales_list += page_sales
+        
+        
+        
+        
+    sales_df = pd.DataFrame.from_dict(sales_list)
+        
+    sales_df.to_csv('zachsales_df.csv')
+        
+    return sales_df
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def sales():
+     
+        '''This function returns a dataframe from a csv thats already in the directory'''
+        
+        
+    sales = pd.read_csv('zachsales_df.csv', index_col=0)
+        
+    return sales
+        
+
+
+def merge_dataframes():
+    
+    '''This function takes in the previous 3 functions above, renames the sales column to give a common id of "store_id"
+    and then merges the sales and items data frames followed by a merge of the newly formed sales_merge data frame and store data frames.
+    A new data frame called store_item_sales is return'''
+    
+    global items
+    global stores
+    global sales
+    
+    items = items()
+    stores = stores()
+    sales = sales()
+  
+    sales.columns = ['item_id', 'sale_amount', 'sale_date', 'sale_id', 'store_id']
+    
+    sales_merge = pd.merge(items, sales, how='left', on=['item_id'], suffixes=['', '_'])
+    cols = ['item_id', 'item_brand', 'item_name', 'item_price', 'sale_amount', 'sale_date', 'sale_id', 'store_id', 'item_upc12', 'item_upc14']
+    sales_merge = sales_merge[cols]
+    
+    store_item_sales = pd.merge(stores, sales_merge, how='left', on=['store_id'], suffixes=['', '_'])
+    cols = ['item_id', 'item_brand', 'item_name', 'item_price', 'sale_amount', 'sale_date', 'sale_id', 'item_upc12', 'item_upc14', 'store_address', 
+    'store_city', 'store_state', 'store_zipcode', 'store_id']
+    store_item_sales = store_item_sales[cols]
+    
+    
+    return store_item_sales
